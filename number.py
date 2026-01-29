ec12a038ce77d72a1206fc2a2012f3c812dce181
ely1570 <ely1570@kookmin.ac.kr>
2026-01-29 13:36:04 +0900

    feat:number and code

--- number.py
+++ number.py
@@ -1,4 +1,4 @@
-number = 10
+number = 2500
 
 print(number + 5)   # 덧셈
 print(number - 3)   # 뺄셈
@@ -6,3 +6,4 @@ print(number * 2)   # 곱셈
 print(number / 4)   # 나눗셈
 print(number // 4)  # 몫
 print(number % 4)   # 나머지
+print(round(number))
